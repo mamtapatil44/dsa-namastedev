0b2e38cecb91f4a8151b928708ef1bad98b4d6ce
Mamta Patil <mamtapatil44@gmail.com>
2025-06-05 13:43:15 -0400

    remove element

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -14,3 +14,18 @@ console.log(arr)
  return x+1;
 }
 console.log("duplicate numbders" ,removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
+
+function RemoveElement(arr,value){
+    let n = arr.length;
+    let x= 0;
+    for(let i =0 ;i <n;i++){
+        if(arr[i] !== value){
+            arr[x] = arr[i];
+            x = x+1
+        }
+
+    }
+  return x;
+}
+
+console.log("RemoveElement" ,RemoveElement([0,0,1,1,1,2,2,3,3,4],1))
