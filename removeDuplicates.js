1b146dfa5e81b7247b7da527e7091ae7975fbf3f
Mamta Patil <mamtapatil44@gmail.com>
2025-06-06 11:02:01 -0400

    remove element

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -1,31 +1,52 @@
-
-
-function removeDuplicates(arr){
-    let n = arr.length;
-    let x =0 ;
-    for(let i =0 ; i <n ;i++){
-        if(arr[i] > arr[x]){
-            x = x+1;
-            arr[x] =arr[i]
-        }
-
+function removeDuplicates(arr) {
+  let n = arr.length;
+  let x = 0;
+  for (let i = 0; i < n; i++) {
+    if (arr[i] > arr[x]) {
+      x = x + 1;
+      arr[x] = arr[i];
     }
-console.log(arr)
- return x+1;
+  }
+  console.log(arr);
+  return x + 1;
 }
-console.log("duplicate numbders" ,removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
+console.log(
+  "duplicate numbders",
+  removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4])
+);
 
-function RemoveElement(arr,value){
-    let n = arr.length;
-    let x= 0;
-    for(let i =0 ;i <n;i++){
-        if(arr[i] !== value){
-            arr[x] = arr[i];
-            x = x+1
-        }
-
+function RemoveElement(arr, value) {
+  let n = arr.length;
+  let x = 0;
+  for (let i = 0; i < n; i++) {
+    if (arr[i] !== value) {
+      arr[x] = arr[i];
+      x = x + 1;
     }
+  }
   return x;
 }
 
-console.log("RemoveElement" ,RemoveElement([0,0,1,1,1,2,2,3,3,4],1))
+console.log("RemoveElement", RemoveElement([0, 0, 1, 1, 1, 2, 2, 3, 3, 4], 1));
+
+function reverseStringofArr(s) {
+  return s.reverse();
+}
+
+console.log(
+  "reverseStringofArr",
+  reverseStringofArr(["h", "e", "l", "l", "o"])
+);
+
+function reverseStringofArrWIthValue(s) {
+  for (let i = 0; i < Math.floor(s.length / 2); i++) {
+    let temp = s[i];
+    s[i] = s[s.length- 1 - i];
+    s[s.length - 1 - i] = temp;
+  }
+  return s;
+}
+console.log(
+    "reverseStringofArrWIthValue===",
+    reverseStringofArrWIthValue(["h", "e", "l", "l", "o"])
+  );
