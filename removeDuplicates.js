d69b8bf1c47e71b94a751ca5dbf6b379fc5138c8
Mamta Patil <mamtapatil44@gmail.com>
2025-06-06 11:51:59 -0400

    move zero

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -50,3 +50,26 @@ console.log(
     "reverseStringofArrWIthValue===",
     reverseStringofArrWIthValue(["h", "e", "l", "l", "o"])
   );
+
+
+  function moveZeros(nums) {
+    let x= 0;
+
+    for(let i =0 ;i < nums.length;i++){
+        if(nums[i] > 0){
+            nums[x] = nums[i];
+            x= x+1;
+
+        }
+
+    }
+
+    for(let i =x ;i <nums.length;i++){
+       nums[i] =0
+    }
+    return nums;
+    
+};
+
+
+console.log("mpving zeros= ",moveZeros([0,1,0,3,12]))
