a69d0a1b9e6e3a6b806c56f99d138df728cbc70f
Mamta Patil <mamtapatil44@gmail.com>
2025-06-06 12:24:27 -0400

    maxconsucative ones

---
+++ maxConsucatives.js
@@ -0,0 +1,16 @@
+function maxCOnsucativeOnsCOunt(nums) {
+  let count = 0;
+  let mxCount = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] == 1) {
+      count++;
+    } else {
+      mxCount = Math.max(count, mxCount);
+      count = 0;
+    }
+  }
+  mxCount = Math.max(count, mxCount);
+  return mxCount;
+}
+
+console.log(maxCOnsucativeOnsCOunt([1, 1, 0, 1, 1, 1]));
