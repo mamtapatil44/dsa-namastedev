c1abb7cfd508c121ec8b572d02547088c9fd4478
Mamta Patil <mamtapatil44@gmail.com>
2025-06-13 12:37:07 -0400

    dsa sum

--- recursion/sum.js
+++ recursion/sum.js
@@ -4,3 +4,12 @@ function sum(n){
     return n+ sum(n-1)
 }
 console.log(sum(10))
+
+let arr = [5,3,2,10,1]
+function sumarr(n){
+    if (n === 0) return arr[n];
+    return arr[n] + sumarr(n-1);
+
+}
+
+console.log("arry smu",sumarr((arr.length - 1)))
