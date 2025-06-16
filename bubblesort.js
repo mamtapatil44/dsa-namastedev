fe177b8b16cc8f2057db5dd9ccee1ef4929d8f94
Mamta Patil <mamtapatil44@gmail.com>
2025-06-16 13:01:13 -0400

    bubble sort

---
+++ bubblesort.js
@@ -0,0 +1,16 @@
+//  buuble sort iterate loop n-1 times;
+
+function bubbleSort(arr) {
+    let n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+
+  return arr;
+}
+
+console.log("bubble sort ",bubbleSort([5,2,4,1]))
